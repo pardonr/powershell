691d4931824c7fa3b86c8c3da9e9d58b6d58eb14
Rainer Pardon <rainer.pardon@pardonnet.com>
2026-06-12 09:44:24 +0200

    AD Ordner hinzugefügt

---
+++ Scripte/AD/DC_promoten.ps1
@@ -0,0 +1,18 @@
+$dsrmPassword = ConvertTo-SecureString "Phoebe_3Q" -AsPlainText -Force
+
+Import-Module ADDSDeployment
+
+Install-ADDSDomainController `
+-NoGlobalCatalog:$false `
+-CreateDnsDelegation:$false `
+-CriticalReplicationOnly:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainName "corp.pri" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-ReplicationSourceDC "dc01.corp.pri" `
+-SiteName "Default-First-Site-Name" `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true `
+-SafeModeAdministratorPassword $dsrmPassword
