35d780a7c1f51b6c9c182614fd75f6440bc2659f
Rainer Pardon <rainer.pardon@pardonnet.com>
2026-06-16 16:54:45 +0200

    Header hinzugefügt

--- Scripte/general_commands/Computerinfo_incl_Patchstand.ps1
+++ Scripte/general_commands/Computerinfo_incl_Patchstand.ps1
@@ -1,3 +1,8 @@
+# Author: Rainer Pardon
+# Titel: Computerinfo_incl_Patchstand
+# Version: 1.0
+# Datum: 16.06.2026
+
 Get-ComputerInfo | Select-Object OsName .\Default.rdp
 
 Get-CimInstance -Class Win32_ComputerSystem | Select-Object *
